f237b0ceaa80fea4d779020ffae01fb1e7b9c608
Raphael Leroy <raphael.leroy@epita.fr>
2022-10-05 16:19:07 +0200

    link.sh done and added bunch of other configs

--- link.sh
+++ link.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-if [ "$AFS_DIR" = "" ]
-then
-    exit 0
-fi
+SCRIPT_PATH=$(dirname "$0")
+
+for f in "$SCRIPT_PATH"/*
+do
+    THISCONF=$(basename "$f")
+    if [ "$THISCONF" = 'link.sh' ] || [ "$THISCONF" = 'config' ] ||
+        [ "$THISCONF" = 'README.md' ]
+    then
+        continue
+    fi
+    echo "$THISCONF"
+    rm -rf "$HOME/.$THISCONF"
+    ln -s "$SCRIPT_PATH/$THISCONF" "$HOME/.$THISCONF"
+    # echo "$SCRIPT_PATH/$THISCONF will become $HOME/$THISCONF"
+done
+
+for f in "$SCRIPT_PATH"/config/*
+do
+    THISCONF=$(basename "$f")
+    rm -rf "$HOME/.config/$THISCONF"
+    ln -s "$SCRIPT_PATH/config/$THISCONF" "$HOME/.config/$THISCONF"
+    # echo "$THISCONF"
+done
